dbbab37cce25062f33ea34f620e0f83df34cc4a7
nuttchai <nuttchai@outlook.com>
2021-03-07 21:51:14 +0000

    milestone1:v03

---
+++ tcpclient.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+        "bufio"
+        "fmt"
+        "net"
+        "os"
+)
+
+func main() {
+        arguments := os.Args
+        if len(arguments) == 1 {
+                fmt.Println("Please provide host:port.")
+                return
+        }
+
+        CONNECT := arguments[1]
+        c, err := net.Dial("tcp", CONNECT)
+        if err != nil {
+                fmt.Println(err)
+                return
+        }
+
+        for {
+                reader := bufio.NewReader(os.Stdin)
+                fmt.Print(">> ")
+                text, _ := reader.ReadString('\n')
+                fmt.Fprintf(c, text+"\n")
+        }
+}
