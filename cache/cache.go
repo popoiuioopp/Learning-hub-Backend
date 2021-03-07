efdcd717a17f1d8a78193a93d2b475d4f54c6f92
popoiuioopp <biggerbosssuper@gmail.com>
2021-03-07 13:46:21 +0700

    Pingpong redis

--- cache/cache.go
+++ cache/cache.go
@@ -1,10 +1,13 @@
 package main
 
 import (
-	"database/sql"
 	"fmt"
 
 	_ "github.com/go-sql-driver/mysql"
+
+	"database/sql"
+
+	"github.com/go-redis/redis"
 )
 
 // SQLHandler refers to the connection to the database.
@@ -16,25 +19,45 @@ var sqliteHandler SQLHandler
 
 // User struct created when there is a signal to create user.
 type User struct {
+	UserID   int    `json:"userID"`
 	Username string `json:"username"`
 	Password string `json:"password"`
 }
 
 //Check for the error
-func checkErr(err error) {
+func checkErr(err error) int {
 	if err != nil {
 		fmt.Println(err)
-		panic(err.Error())
+		return 1
 	}
+	return 0
 }
 
 func main() {
+	redisClient := newClient()
 
-	db, err := sql.Open("mysql", "learninghub:FgTQTzNM62cC63K@tcp(139.59.106.148:3306)/learninghub")
+	result, err := ping(redisClient)
 	checkErr(err)
+	fmt.Println(result)
+}
 
-	fmt.Println("Connected to database")
-	sqliteHandler.Conn = db
-	defer db.Close()
+func newClient() *redis.Client {
+	redisClient := redis.NewClient(&redis.Options{
+		Network:  "tcp",
+		Addr:     "localhost:6379",
+		Password: "", // no password
+		DB:       0,  // default DB
+	})
 
+	return redisClient
+}
+
+//Check connection between redis and the program.
+func ping(client *redis.Client) (string, error) {
+	result, err := client.Ping().Result()
+
+	if checkErr(err) == 1 {
+		return "", err
+	}
+	return result, nil
 }
--- sqlStatements/TableGen.sql
+++ sqlStatements/TableGen.sql
@@ -1,16 +1,22 @@
 use learninghub;
 
 CREATE TABLE User (
-		userID int not null AUTO_INCREMENT primary key,
+	userID int not null AUTO_INCREMENT primary key,
         username varchar(25) not null,
         password varchar(25)
-        );
+);
 
 CREATE TABLE Flashcard_instance (
 	flashcardId int not null AUTO_INCREMENT primary key,
-    deckId int,
-    term varchar(25) not null,
-    definition varchar(255) not null,
+	deckId int,
+	term varchar(25) not null,
+	definition varchar(255) not null,
 	userID int,
     foreign key (userID) references User(userID)
+);
+
+CREATE TABLE Deck_instance (
+	deckId int not null AUTO_INCREMENT primary key,
+	deckName varchar(50) not null, 
+	dateCreate DATETIME
 );
