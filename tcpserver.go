93068bb92701811ace92df9bb698ba589b9541ab
nuttchai <nuttchai@outlook.com>
2021-03-07 18:44:34 +0000

    milestone1:v02

---
+++ tcpserver.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"net"
+)
+
+func main() {
+	//listen
+	//accept
+	//handle connection -> thread
+	dstream, err := net.Listen("tcp", ":8080")
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer dstream.Close()
+
+	for {
+		con, err := dstream.Accept()
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		go handle(con)
+	}
+
+}
+
+func handle(con net.Conn) {
+	for {
+		data, err := bufio.NewReader(con).ReadString('\n')
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		fmt.Println(data)
+	}
+	con.Close()
+}
