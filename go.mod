fad2e6350c0d9c046b0260418d70d78e70b7368c
Visarut Techatanchai <61011358@kmitl.ac.th>
2021-03-13 21:25:45 +0700

    New CLI
    Reference : https://github.com/plutov/packagemain/tree/master/20-tcp-chat/chat

--- go.mod
+++ go.mod
@@ -1,8 +1,3 @@
-module github.com/popoiuioopp/Learning-hub-Backend
+module example.com
 
 go 1.16
-
-require (
-	github.com/go-sql-driver/mysql v1.5.0 // indirect
-	golang.org/x/time v0.0.0-20210220033141-f8bda1e9f3ba
-)
--- main.go
+++ main.go
@@ -1,85 +1,29 @@
 package main
 
 import (
-	"database/sql"
-	"fmt"
-
-	_ "github.com/go-sql-driver/mysql"
+	"log"
+	"net"
 )
 
-// SQLHandler refers to the connection to the database.
-type SQLHandler struct {
-	Conn *sql.DB
-}
-
-var sqliteHandler SQLHandler
-
-// check error
-func checkErr(err error) {
-	if err != nil {
-		fmt.Println(err)
-		panic(err.Error())
-	}
-}
-
-// create Flashcard and store in database
 func main() {
-	// limiter := rate.NewLimiter(10, 1)
-	// http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-	// 	if !limiter.Allow() {
-	// 		w.WriteHeader(http.StatusTooManyRequests)
-	// 		return
-	// 	}
-	// 	fmt.Fprintln(w, "Hello, World")
-	// })
-	// log.Fatal(http.ListenAndServe(":8080", nil))
-	db, err := sql.Open("mysql", "learninghub:FgTQTzNM62cC63K@tcp(139.59.106.148:3306)/learninghub")
-	checkErr(err)
-	sqliteHandler.Conn = db
+	s := newServer()
+	go s.run()
 
-	create()
-
-	var regisVerifyPass string
-	fmt.Scanln(&regisVerifyPass)
-}
-
-func create() {
-
-	type FlashCard struct {
-		Term       string
-		Definition string
+	listener, err := net.Listen("tcp", ":8888")
+	if err != nil {
+		log.Fatalf("unable to start server: %s", err.Error())
 	}
 
-	fmt.Println(">>>>>>>Create FlashCard")
+	defer listener.Close()
+	log.Printf("server started on :8888")
 
-	fmt.Println("Flashcard name : ")
-	var namefc string
-	fmt.Scanln(&namefc)
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Printf("failed to accept connection: %s", err.Error())
+			continue
+		}
 
-	fmt.Println("Number of Flashcard : ")
-	var numfc int
-	fmt.Scanln(&numfc)
-	var slice []FlashCard
-	fmt.Println(slice)
-
-	var temp FlashCard
-	for i := 0; i < numfc; i++ {
-		fmt.Println("Term : ")
-		fmt.Scanln(&temp.Term)
-		fmt.Println("Definition : ")
-		fmt.Scanln(&temp.Definition)
-		slice = append(slice, temp)
-	}
-	fmt.Println(slice)
-	fmt.Println(len(slice))
-
-	for _, element := range slice {
-		// fmt.Println(index, element.Term)
-		sqlStatement := `
-		INSERT INTO Flashcard_instance(deckId,term,definition,userID)
-		VALUES(1,?,?,1)
-		`
-		_, err := sqliteHandler.Conn.Exec(sqlStatement, element.Term, element.Definition)
-		checkErr(err)
+		go s.newClient(conn)
 	}
 }
