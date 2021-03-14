0d0f073fb1b416983a5867443ac68c9e99b5f41f
Pongpich <biggerbosssuper@gmail.com>
2021-03-14 14:48:43 +0700

    update

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 )
 
 func main() {
+	newDBConn()
 	s := newServer()
 	go s.run()
 
--- server.go
+++ server.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"bufio"
+	"database/sql"
 	"fmt"
 	"log"
 	"net"
@@ -12,6 +12,12 @@ type server struct {
 	rooms    map[string]*room
 	commands chan command
 }
+
+type SQLHandler struct {
+	Conn *sql.DB
+}
+
+var sqliteHandler SQLHandler
 
 func newServer() *server {
 	return &server{
@@ -54,10 +60,23 @@ func (s *server) newClient(conn net.Conn) {
 		conn:     conn,
 		nick:     "anonymous",
 		status:   "0",
-		commands: s.commands, 
+		commands: s.commands,
 	}
 
 	c.readInput()
+}
+
+func newDBConn() {
+
+	db, err := sql.Open("mysql", "learninghub:FgTQTzNM62cC63K@tcp(139.59.106.148:3306)/learninghub")
+	if err != nil {
+		return
+	}
+
+	fmt.Println("Connected to database")
+	sqliteHandler.Conn = db
+
+	defer db.Close()
 }
 
 func (s *server) nick(c *client, nick string) {
@@ -119,38 +138,42 @@ func (s *server) startGame(c *client) {
 	//start game
 }
 
-func checkDeckExist(db *sql.DB) int {
+func checkDeckExist(db *sql.DB, name string) int {
 	var result int
 	statement := `SELECT COUNT(*) FROM learninghub.Deck_instance where learninghub.Deck_instance.deckName = ?;`
 	rows, err := db.Query(statement, name)
 	if err != nil {
-		return
+		return 1
 	}
 	for rows.Next() {
 		err = rows.Scan(&result)
+		fmt.Print(result)
 		if err != nil {
-			return
+			return 1
 		}
 	}
 	return result
 }
 
-func (s *server) createDeck(c *client) {
+func (s *server) createDeck(c *client, db *sql.DB, deckname string) {
 	//create deck
 	if checkDeckExist(db, deckname) == 0 {
 		sqlStatement := `INSERT INTO Deck_instance(deckName, dateCreate) VALUES(?, NOW())`
 		_, err := db.Exec(sqlStatement, deckname)
 
-		checkErr(err)
+		if err != nil {
+			return
+		}
 
 	} else {
-		fmt.Println("This Deck Name Already Used.")
+		fmt.Println("Room already existed!...")
+		return
 	}
 }
 
 func (s *server) createfc(c *client, namefc string, total string) {
 	//create fc
-	
+
 }
 
 func (s *server) login(c *client, username string, pass string) {
@@ -171,7 +194,7 @@ func (s *server) listRooms(c *client) {
 }
 
 func (s *server) msg(c *client, args []string) {
-	msg := strings.Join(args[1:len(args)], " ")
+	msg := strings.Join(args[1:], " ")
 	c.room.broadcast(c, c.nick+": "+msg)
 }
 
